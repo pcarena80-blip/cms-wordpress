f5b5ce3e9f8ecf093b121698cf3bcb1a38dd1030
pcarena80-blip <pcarena80@gmail.com>
2025-08-04 10:40:42 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: process.env.NEXT_PUBLIC_WORDPRESS_API_HOSTNAME,
-        port: "",
+        protocol: 'https',
+        hostname: 'demo.wpgraphql.com',
+        pathname: '/**',
       },
     ],
   },
