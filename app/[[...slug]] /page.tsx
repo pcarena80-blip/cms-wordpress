f9099cd9157d24af4755c3c2ed6741233b59466e
pcarena80-blip <pcarena80@gmail.com>
2025-08-04 13:06:02 +0500

    Update page.tsx

--- app/[[...slug]] /page.tsx
+++ app/[[...slug]] /page.tsx
@@ -1,21 +1,23 @@
+// app/[slug]/page.tsx
+
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { print } from "graphql/language/printer";
 
 import { setSeoData } from "@/utils/seoData";
-
 import { fetchGraphQL } from "@/utils/fetchGraphQL";
 import { ContentInfoQuery } from "@/queries/general/ContentInfoQuery";
 import { ContentNode } from "@/gql/graphql";
 import PageTemplate from "@/components/Templates/Page/PageTemplate";
-import { nextSlugToWpSlug } from "@/utils/nextSlugToWpSlug";
 import PostTemplate from "@/components/Templates/Post/PostTemplate";
 import { SeoQuery } from "@/queries/general/SeoQuery";
+import { nextSlugToWpSlug } from "@/utils/nextSlugToWpSlug";
 
 type Props = {
   params: { slug: string };
 };
 
+// SEO Metadata Generator
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = nextSlugToWpSlug(params.slug);
   const isPreview = slug.includes("preview");
@@ -25,12 +27,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     {
       slug: isPreview ? slug.split("preview/")[1] : slug,
       idType: isPreview ? "DATABASE_ID" : "URI",
-    },
+    }
   );
 
-  if (!contentNode) {
-    return notFound();
-  }
+  if (!contentNode) return notFound();
 
   const metadata = setSeoData({ seo: contentNode.seo });
 
@@ -39,22 +39,25 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     alternates: {
       canonical: `${process.env.NEXT_PUBLIC_BASE_URL}${slug}`,
     },
-  } as Metadata;
+  };
 }
 
+// For static generation fallback
 export function generateStaticParams() {
   return [];
 }
 
+// Dynamic page renderer based on WP content type
 export default async function Page({ params }: Props) {
   const slug = nextSlugToWpSlug(params.slug);
   const isPreview = slug.includes("preview");
+
   const { contentNode } = await fetchGraphQL<{ contentNode: ContentNode }>(
     print(ContentInfoQuery),
     {
       slug: isPreview ? slug.split("preview/")[1] : slug,
       idType: isPreview ? "DATABASE_ID" : "URI",
-    },
+    }
   );
 
   if (!contentNode) return notFound();
