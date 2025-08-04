e5f6d5905683504724c67faeaaae4c8434e151a1
pcarena80-blip <pcarena80@gmail.com>
2025-08-04 15:47:25 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true
+  },
+  // ADD THIS:
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
+};
+
+module.exports = nextConfig;
+
 const nextConfig = {
   experimental: {
     appDir: true, 
