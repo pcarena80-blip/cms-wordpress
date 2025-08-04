66a10a8a06286655cb4df5f1269e4725f939faab
pcarena80-blip <pcarena80@gmail.com>
2025-08-04 10:58:44 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // 👈 Enable support for app directory inside /src
+  },
   images: {
     remotePatterns: [
       {
